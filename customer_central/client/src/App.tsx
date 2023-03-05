72ea2e8c004c97c78e80dc77cb81486414e3d7f2
Nicolas <nicolasmuller.barros@gmail.com>
2023-03-04 21:17:57 -0300

    setting mui theme

--- customer_central/client/src/App.tsx
+++ customer_central/client/src/App.tsx
@@ -1,10 +1,26 @@
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 import Component from "@/class/Component"
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material"
+
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+})
 
 class App extends Component {
 	component: T.Component = App
 
 	static build(): JSX.Element {
-		return <></>
+		return <>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				asd
+			</ThemeProvider>
+		</>
 	}
 }
 

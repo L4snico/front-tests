b3b9161cfde9aaea7e501be50fbdf7e29f0bba12
Nicolas <nicolasmuller.barros@gmail.com>
2023-03-05 16:16:01 -0300

    implementing sign in page

--- customer_central/client/src/App.tsx
+++ customer_central/client/src/App.tsx
@@ -5,22 +5,45 @@ import "@fontsource/roboto/700.css"
 import Component from "@/class/component"
 import { createTheme, ThemeProvider, CssBaseline } from "@mui/material"
 import Routes from "@/router/routes"
-
-const theme = createTheme({
-	palette: {
-		mode: "dark",
-	},
-})
+import React from "react"
+import ColorModeContext from "./context/color_mode"
 
 class App extends Component {
 	element: T.Component = App
 
 	static build(): JSX.Element {
+		const [mode, setMode] = React.useState<"light" | "dark">("dark")
+		const colorMode = React.useMemo(
+			() => ({
+				toggleColorMode: () => setMode((prevMode) => prevMode === "light" ? "dark" : "light")
+			}),
+			[],
+		)
+
+		const theme = React.useMemo(
+			() => (
+				createTheme({
+					palette: {
+						mode,
+						primary: {
+							main: mode === "light" ? "#06283D" : "#47B5FF",
+						},
+						secondary: {
+							main: mode === "light" ? "#47B5FF" : "#06283D",
+						},
+					},
+				})
+			),
+			[mode],
+		)
+		
 		return <>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Routes />
-			</ThemeProvider>
+			<ColorModeContext.Provider value={colorMode}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes />
+				</ThemeProvider>
+			</ColorModeContext.Provider>
 		</>
 	}
 }
--- customer_central/client/src/page/auth/sign_in/index.tsx
+++ customer_central/client/src/page/auth/sign_in/index.tsx
@@ -1,11 +1,96 @@
 import Component from "@/class/component";
+import { AppBar, Box, Button, Container, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, Link, OutlinedInput, TextField, Toolbar, Typography, useTheme } from "@mui/material";
+import { DarkMode, Domain, LightMode, Visibility, VisibilityOff } from "@mui/icons-material";
+import ColorModeContext from "@/context/color_mode";
+import React from "react";
 
 class SignInPage extends Component {
     element: T.Component = SignInPage
 
     static build(): JSX.Element {
+        const theme = useTheme()
+        const colorMode = React.useContext(ColorModeContext)
+        const [showPassword, setShowPassword] = React.useState(false)
+        
         return <>
-            Sign In Page
+            <Grid container sx={{ height: "100vh" }}>
+                <Grid item container xs={12} md={6} lg={6} sx={{ height: "100vh" }}>
+                    <Grid item xs={12}>
+                        <AppBar position="static" variant="outlined" elevation={0} sx={{ border: "none" }} color="transparent">
+                            <Toolbar>
+                                <Domain sx={{ mr: 1 }} color="primary" />
+                                <Typography sx={{ flexGrow: 1 }}>Domain</Typography>
+                                <IconButton onClick={colorMode.toggleColorMode}>
+                                    {theme.palette.mode === "light" ? <DarkMode /> : <LightMode />}
+                                </IconButton>
+                            </Toolbar>
+                        </AppBar>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Container maxWidth="xs">
+                            <Box 
+                                sx={{
+                                    padding: 5,
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <Typography variant="h5">Customer Central</Typography>
+                                <Box component="form" noValidate sx={{ mt: 3, width: "100%" }}>
+                                    <TextField label="Email" fullWidth margin="normal" />
+                                    <FormControl fullWidth margin="normal">
+                                        <InputLabel htmlFor="password">Password</InputLabel>
+                                        <OutlinedInput
+                                            id="password"
+                                            label="Password"
+                                            type={showPassword ? "text" : "password"}
+                                            endAdornment={
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        aria-label="toggle password visibility"
+                                                        onClick={() => setShowPassword(!showPassword)}
+                                                        edge="end"
+                                                    >
+                                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            }
+                                        />
+                                    </FormControl>
+                                    <Link href="#" variant="body2">Forgot password?</Link>
+                                    <Button
+                                        variant="contained"
+                                        disableElevation
+                                        fullWidth
+                                        sx={{ mt: 2 }}
+                                        size="large"
+                                    >
+                                        Sign In
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Container>
+                    </Grid>
+                    <Grid item xs={12} sx={{ position: "relative" }}>
+                        <AppBar color="transparent" elevation={0} sx={{ position: "absolute", top: "auto", bottom: 0 }}>
+                            <Toolbar>
+                                <Typography variant="body2" color="GrayText">Copyright © Domain 2023.</Typography>
+                            </Toolbar>
+                        </AppBar>
+                    </Grid>
+                </Grid>
+                <Grid
+                    item
+                    sm={false}
+                    md={6}
+                    sx={{
+                        backgroundImage: "url(/src/assets/images/domain_background.jpg)",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                    }}
+                />
+            </Grid>
         </>
     }
 }
